5544af29a04af11c7a54fcd6b20767be0adc275d
Aleksey <aay92@yandex.ru>
2023-08-22 20:13:00 +0400

    Initial Commit

---
+++ Messenger_SwiftUI/Messenger_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Messenger_SwiftUIApp.swift
+//  Messenger_SwiftUI
+//
+//  Created by Aleksey Alyonin on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Messenger_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
